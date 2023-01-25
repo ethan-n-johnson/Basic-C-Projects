afbdd9996d12c346e61cf87c856978d02fa58807
Ethan Johnson <ethansoccer111@gmail.com>
2023-01-24 20:57:15 -0600

    first commit

--- C/test.c
+++ C/test.c
@@ -1,1 +1,20 @@
-//Temp
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(void) 
+{ 
+   int x = 0;
+   int y = 0;
+   int size = 10;
+   int array[size][size];
+
+   for(int i = 0; i < size; i++){
+      x = x + 1;
+      y = y + 2;
+      printf("%c ", array[x][y]);
+      printf("\n");
+   }
+
+   system("pause");
+   return 0; 
+}  
