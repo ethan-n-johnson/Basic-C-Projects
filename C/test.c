7e6b4a493405585ab89e2358526d76e3f2780f49
ethan-n-johnson <ethansoccer111@gmail.com>
2024-08-13 18:18:59 -0500

    no changes

--- C/test.c
+++ C/test.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+
+int ShiftRight(int bitA, int shift_amount)
+{
+      return bitA >> shift_amount;
+}
+int ShiftLeft(int bitA, int shift_amount)
+{
+      return bitA << shift_amount;
+}
+
 int main()
 {
-      char Array1[100] = "banana";
-      char Array2[100] = "bread";
-      strcpy(Array2, Array1);
-
-      printf("%s", Array2);
+      int bit5 = 101;
+      int shift_amount = 1;
+      printf("%d\n", bit5);
+      printf("Shifted Left by %d: %d\n", shift_amount, ShiftLeft(bit5, shift_amount));
+      printf("Shifted Right by %d: %d\n", shift_amount, ShiftRight(bit5, shift_amount));
 }
